4a4375147b71832fcc09058680b9068422f44dce
SOMNATH BISWAS <108716703+SomnathBiswas@users.noreply.github.com>
2023-05-13 13:42:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+// toggle icon navbar
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// // scroll section active link
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id+ ']').classList.add('active');
+                
+            });
+        };
+    });
+   // sticky navbar
+
+    let header=document.querySelecror('header');
+
+    header.classList.toggle('sticky', window.scrollY>100);
+
+    /*=======remove toggle icon and navbar when click navbar link (scroll)==============*/
+   
+    // menuIcon.classList.remove('bx-x');
+    // navbar.classList.remove('active');
+    navlinks.forEach(link => {
+        link.addEventListener('click', () => {
+          navbar.classList.remove('active');
+          menuIcon.classList.remove('bx-x');
+          navbar.blur();
+        });
+      });
+      
+    
+};
+
+
+/*=========scroll reveal============*/
+ScrollReveal({ 
+    reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
+
+
+
+/*=========type js============*/
+const typed = new Typed('.multiple-text',{
+    strings:['Full-Stack Developer', 'Artist'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
